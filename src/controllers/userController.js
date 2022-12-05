1ab28cdc275c27340fac3b02205b3f60dedc2874
PhuforWork <vqphu97@gmail.com>
2022-12-05 10:39:44 +0700

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,79 @@
 const sequelize = require("../models/index");
 const init_models = require("../models/init-models");
+const { successCode, failCode, errorCode } = require("../utils/respone");
 const model = init_models(sequelize);
 
+//Read all user
 const getuser = async (req, res) => {
   let data = await model.users.findAll();
   res.send(data);
 };
+// Read user by id
+const getUserId = async (req, res) => {
+  let { id } = req.params;
+  let data = await model.users.findByPk(id);
+  res.send(data);
+};
+// Login user
+const loginUser = async (req, res) => {
+  let { user_name, _password } = req.body;
+  const checkUser = await model.users.findOne({
+    where: {
+      user_name,
+    },
+  });
+  if (checkUser) {
+    if ((checkUser._password = _password)) {
+      successCode(res, "", "Login successfully");
+    } else {
+      failCode(res, "", "Email không đúng");
+    }
+  } else {
+    errorCode(res, "", "Login fail");
+  }
+};
+// register
+const sigUp = async (req, res) => {
+  try {
+    let { user_name, email, _password, confirm_password, image_url } = req.body;
+    let data = { user_name, email, _password, confirm_password, image_url };
+    const checkUsername = model.users.findOne({
+      where: {
+        user_name,
+      },
+    });
+    if (checkUsername) {
+      failCode(res, "", "User name already used");
+    } else {
+      await model.users.create(data);
+      successCode(res, "", "Sig up successfully");
+    }
+  } catch (error) {
+    errorCode(res, "Sig up failed");
+  }
+};
 
-module.exports = { getuser };
+const updateUser = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let { user_name, email, _password, confirm_password, image_url } = req.body;
+    // check data user
+    let checkUser = model.users.findByPk(id);
+    let update_User = {
+      user_name,
+      email,
+      _password,
+      confirm_password,
+      image_url,
+    };
+    if (checkUser) {
+      await model.users.update(update_User, { where: { id_user: id } });
+      successCode(res, update_User, "Update successfully");
+    } else {
+      failCode(res, "", "Update failed");
+    }
+  } catch (error) {
+    errorCode(res, "Error 500");
+  }
+};
+module.exports = { getuser, loginUser, sigUp, updateUser, getUserId };
--- src/routes/userRoute.js
+++ src/routes/userRoute.js
@@ -1,11 +1,25 @@
 const express = require("express");
-const { getuser } = require("../controllers/userController");
+const {
+  getuser,
+  loginUser,
+  sigUp,
+  updateUser,
+  getUserId,
+} = require("../controllers/userController");
 const userRoute = express.Router();
 
-// get
+// get all user
 userRoute.get("/get-user", getuser);
+// get user by id
+userRoute.get("/get-user/:id", getUserId);
 // post
+// login
+userRoute.post("/login-user", loginUser);
+// register
+userRoute.post("/sig-up-user", sigUp);
 // put
+// update user
+userRoute.put("/update-user", updateUser);
 // delete
 
 module.exports = userRoute;
