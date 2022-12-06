9b1a3b089cda6185f39c93d9ec14a6ede432258f
PhuforWork <vqphu97@gmail.com>
2022-12-06 14:40:25 +0700

    update3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -90,14 +90,18 @@ const update_img = async (req, res) => {
   let checkUser = model.users.findByPk(id);
   if (checkUser) {
     console.log(process.cwd() + "/" + req.file.path);
-    await fs.readFile(process.cwd() + "/" + req.file.path, async(err, data) => {
-      let fileName = `data:${req.file.mimetype};base64,${Buffer.from(
-        data
-      ).toString("base64")}`;
-      fs.unlinkSync(process.cwd() + "/" + req.file.path);
-      let update_user = {image_url:fileName};
-      await model.users.update(update_user, { where: { id_user: id } });
-    });
+    await fs.readFile(
+      process.cwd() + "/" + req.file.path,
+      async (err, data) => {
+        let fileName = `data:${req.file.mimetype};base64,${Buffer.from(
+          data
+        ).toString("base64")}`;
+        fs.unlinkSync(process.cwd() + "/" + req.file.path);
+        let update_user = { image_url };
+        update_user.image_url = fileName;
+        await model.users.update(update_user, { where: { id_user: id } });
+      }
+    );
   }
 };
 
