b8ab6bd1cc0cc9a90d30c557a559b450ebbd01ae
PhuforWork <vqphu97@gmail.com>
2022-12-06 12:14:03 +0700

    update2

--- src/Middlewares/upload.js
+++ src/Middlewares/upload.js
@@ -1,13 +1,13 @@
 const multer = require("multer");
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null,"/"+"./var/www/src_be_booking/node_bkapp/public/images");
+    cb(null, "./public/images");
   },
   filename: (req, file, cb) => {
     const fileNameNew = Date.now() + "_" + file.originalname;
     cb(null, fileNameNew);
   },
 });
-const upload = multer({storage});
+const upload = multer({ storage });
 
 module.exports = upload;
