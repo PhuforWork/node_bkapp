6cd771ea03660401ee6a3b99b51ea475a1a15689
PhuforWork <vqphu97@gmail.com>
2022-12-06 14:34:09 +0700

    update3

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -86,7 +86,8 @@ const updateUser = async (req, res) => {
 };
 
 const update_img = async (req, res) => {
-  const { id } = req.body;
+  let { id } = req.body;
+  console.log(id);
   console.log(process.cwd() + "/" + req.file.path);
   await fs.readFile(process.cwd() + "/" + req.file.path, (err, data) => {
     let fileName = `data:${req.file.mimetype};base64,${Buffer.from(
