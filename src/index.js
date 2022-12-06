809c8d7fef4bbc9c6a0717fd780d27a41b44271b
PhuforWork <vqphu97@gmail.com>
2022-12-06 13:06:21 +0700

    update2

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const rootRoute = require("../src/routes/index");
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname + "/public"));
+app.use(express.static("."));
 
 app.listen(8081);
 
