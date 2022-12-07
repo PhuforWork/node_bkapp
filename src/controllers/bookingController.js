18969cea9e340e7f195219642b07b3d470537bf1
PhuforWork <vqphu97@gmail.com>
2022-12-07 22:12:44 +0700

    update4

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -9,11 +9,19 @@ const booking_user = async (req, res) => {
   if (check_id) {
     const check_bkUser = await model.booking_info.findAll({
       include: ["select_types", "persionalities"],
-      where: {
-        id_user: id,
-      },
     });
     successCode(res, check_bkUser, "Get success booking of user");
   }
 };
-module.exports = { booking_user };
+
+const select_types = async (req, res) => {
+  let { _selection, id_booking } = req.body;
+  let data = { _selection, id_booking };
+  if (data) {
+    const data_type = await model.select_type.create(data);
+    successCode(res, data_type, "Select type success");
+  } else {
+    failCode(res, "", "Missing types");
+  }
+};
+module.exports = { booking_user, select_types };
--- src/routes/bookingRoute.js
+++ src/routes/bookingRoute.js
@@ -1,8 +1,12 @@
 const express = require("express");
-const { booking_user } = require("../controllers/bookingController");
+const {
+  booking_user,
+  select_types,
+} = require("../controllers/bookingController");
 const upload = require("../Middlewares/upload");
 const bookingRoute = express.Router();
 
-bookingRoute.get("/get-booking/:id",booking_user);
+bookingRoute.get("/get-booking/:id", booking_user);
 
+bookingRoute.post("/add-type", select_types);
 module.exports = bookingRoute;
