fadd3af31c74cdf8a452d10b72f41771ded22c98
PhuforWork <vqphu97@gmail.com>
2022-12-07 22:56:56 +0700

    update4

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -9,22 +9,34 @@ const booking_user = async (req, res) => {
   if (check_id) {
     const check_bkUser = await model.booking_info.findAll({
       include: ["select_types", "persionalities"],
-    //   where: {
-    //     id_user: id,
-    //   },
+    });
+    successCode(res, check_bkUser, "Get success booking of user");
+  }
+};
+const booking_userid = async (req, res) => {
+  let { id } = req.params;
+  let check_id = { id }; //id user
+  if (check_id) {
+    const check_bkUser = await model.booking_info.findAll({
+      include: ["select_types", "persionalities"],
+        where: {
+          id_user: id,
+        },
     });
     successCode(res, check_bkUser, "Get success booking of user");
   }
 };
 
-const select_types = async (req, res) => {
-  let { _selection, id_booking } = req.body;
-  let data = { _selection, id_booking };
+const add_booking = async (req, res) => {
+  let { id } = req.params; // id user
+  let id_user = id;
+  let { start_time, end_time, _date, details } = req.body;
+  let data = { start_time, end_time, _date, details, id_user };
   if (data) {
-    const data_type = await model.select_type.create(data);
-    successCode(res, data_type, "Select type success");
+    const data_bk = await model.select_type.create(data);
+    successCode(res, data_bk, "Select type success");
   } else {
-    failCode(res, "", "Missing types");
+    failCode(res, "", "Missing fields booking");
   }
 };
-module.exports = { booking_user, select_types };
+module.exports = { booking_user, add_booking,booking_userid };
--- src/routes/bookingRoute.js
+++ src/routes/bookingRoute.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const {
   booking_user,
-  select_types,
+  add_booking,
+  booking_userid,
 } = require("../controllers/bookingController");
 const upload = require("../Middlewares/upload");
 const bookingRoute = express.Router();
 
-bookingRoute.get("/get-booking/:id", booking_user);
+bookingRoute.get("/get-booking", booking_user);
 
-bookingRoute.post("/add-type", select_types);
+bookingRoute.get("/get-booking/:id", booking_userid);
+
+bookingRoute.post("/add-booking/:id", upload.none(), add_booking);
 module.exports = bookingRoute;
