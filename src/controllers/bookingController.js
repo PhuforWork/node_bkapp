2d52d263ff80e73eb477a41e8e287b6ae73de5be
PhuforWork <vqphu97@gmail.com>
2022-12-07 20:09:18 +0700

    update4

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -5,13 +5,13 @@ const model = init_models(sequelize);
 
 const booking_user = async (req, res) => {
   let { id } = req.params;
-  let id_user = { id };
-  if (id_user) {
+  let id_booking = { id };
+  if (id_booking) {
     const check_bkUser = await model.booking_info.findOne({
       includes: ["persionalities"],
-      where:{
-        id_user
-      }
+      where: {
+        id_booking,
+      },
     });
     successCode(res, check_bkUser, "Get success booking of user");
   }
