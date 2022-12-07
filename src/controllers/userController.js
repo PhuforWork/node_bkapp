b7026d440a71dbfd6741ebcd42676898023ae207
PhuforWork <vqphu97@gmail.com>
2022-12-07 13:19:54 +0700

    update4

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -137,11 +137,11 @@ const forgot_password = async (req, res) => {
   }
 };
 const change_pass = async (req, res) => {
-  let data = req.body;
-  console.log(data);
+  let { email, _password, confirm_password } = req.body;
+  console.log({ email, _password, confirm_password });
   
   if (_password === confirm_password) {
-    // await model.users.update(data, { where: { email } });
+    await model.users.update(_password, { where: { email } });
     successCode(res, "", "Change password successfully");
   } else {
     errorCode(res, "", "Error 400");
