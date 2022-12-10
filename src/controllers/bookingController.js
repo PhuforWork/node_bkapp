caa0c93959477b79f0d585101b48d7d2a727efb5
PhuforWork <vqphu97@gmail.com>
2022-12-10 11:27:12 +0700

    update

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -105,12 +105,12 @@ const update_slect = async (req, res) => {
   //   },
   // });
   // console.log("checkout", check_select);
-  // const checkheck = await model.select_type.update(data, {
-  //   where: {
-  //     id_user: id,
-  //   },
-  // });
-  // console.log("checkcheck", checkheck);
+  const checkheck = await model.select_type.update(data, {
+    where: {
+      id_user: id,
+    },
+  });
+  console.log("checkcheck", checkheck);
   successCode(res, "", "Update success selection");
 
   // if (check_select) {
