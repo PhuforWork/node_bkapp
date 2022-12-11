3e35daed26ee109183c25c044b91d01110234516
PhuforWork <vqphu97@gmail.com>
2022-12-11 23:36:32 +0900

    update

---
+++ src/models/department_tb.js
@@ -0,0 +1,50 @@
+const Sequelize = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  return department_tb.init(sequelize, DataTypes);
+}
+
+class department_tb extends Sequelize.Model {
+  static init(sequelize, DataTypes) {
+  return super.init({
+    value: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    label: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    id_booking: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'booking_info',
+        key: 'id_booking'
+      }
+    }
+  }, {
+    sequelize,
+    tableName: 'department_tb',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "value" },
+        ]
+      },
+      {
+        name: "id_booking",
+        using: "BTREE",
+        fields: [
+          { name: "id_booking" },
+        ]
+      },
+    ]
+  });
+  }
+}
