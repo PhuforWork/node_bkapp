54e6d3dfe8029ba7b0ca8e11813ce3979d434c13
PhuforWork <vqphu97@gmail.com>
2022-12-12 19:55:32 +0900

    update

---
+++ src/models/department.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  return department.init(sequelize, DataTypes);
+}
+
+class department extends Sequelize.Model {
+  static init(sequelize, DataTypes) {
+  return super.init({
+    value: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    label: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    id_user: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'users',
+        key: 'id_user'
+      }
+    },
+    _name: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    }
+  }, {
+    sequelize,
+    tableName: 'department',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "value" },
+        ]
+      },
+      {
+        name: "id_user",
+        using: "BTREE",
+        fields: [
+          { name: "id_user" },
+        ]
+      },
+    ]
+  });
+  }
+}
--- src/models/init-models.js
+++ src/models/init-models.js
@@ -1,15 +1,15 @@
 const DataTypes = require("sequelize").DataTypes;
 const _booking_info = require("./booking_info");
+const _department = require("./department");
 const _department_tb = require("./department_tb");
-const _persionality = require("./persionality");
 const _select_type = require("./select_type");
 const _select_type_tb = require("./select_type_tb");
 const _users = require("./users");
 
 function initModels(sequelize) {
   const booking_info = _booking_info(sequelize, DataTypes);
+  const department = _department(sequelize, DataTypes);
   const department_tb = _department_tb(sequelize, DataTypes);
-  const persionality = _persionality(sequelize, DataTypes);
   const select_type = _select_type(sequelize, DataTypes);
   const select_type_tb = _select_type_tb(sequelize, DataTypes);
   const users = _users(sequelize, DataTypes);
@@ -20,15 +20,15 @@ function initModels(sequelize) {
   booking_info.hasMany(select_type_tb, { as: "select_type_tbs", foreignKey: "id_booking"});
   booking_info.belongsTo(users, { as: "id_user_user", foreignKey: "id_user"});
   users.hasMany(booking_info, { as: "booking_infos", foreignKey: "id_user"});
-  persionality.belongsTo(users, { as: "id_user_user", foreignKey: "id_user"});
-  users.hasMany(persionality, { as: "persionalities", foreignKey: "id_user"});
+  department.belongsTo(users, { as: "id_user_user", foreignKey: "id_user"});
+  users.hasMany(department, { as: "departments", foreignKey: "id_user"});
   select_type.belongsTo(users, { as: "id_user_user", foreignKey: "id_user"});
   users.hasMany(select_type, { as: "select_types", foreignKey: "id_user"});
 
   return {
     booking_info,
+    department,
     department_tb,
-    persionality,
     select_type,
     select_type_tb,
     users,
