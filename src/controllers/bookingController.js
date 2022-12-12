c2419b00cf62fe64eba8fc2cd6c412f514d96aac
PhuforWork <vqphu97@gmail.com>
2022-12-12 20:15:36 +0700

    update7

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -36,8 +36,9 @@ const add_booking = async (req, res) => {
   let { id } = req.params; // id user
   let id_user = id;
   let { start, end, detail, service } = req.body;
-  console.log(req.body.service._values);
+
   let _values = req.body.service._values;
+  console.log(_values);
   let personality = req.body.personality;
   let department = req.body.department;
   let data = {
