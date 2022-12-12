b02c13d2328e20f83456de4dd9a83e42cac38f7a
PhuforWork <vqphu97@gmail.com>
2022-12-12 20:21:29 +0900

    update

--- src/models/init-models.js
+++ src/models/init-models.js
@@ -3,8 +3,8 @@ const _booking_info = require("./booking_info");
 const _department = require("./department");
 const _department_tb = require("./department_tb");
 const _persionality = require("./persionality");
+const _persionality_tb = require("./persionality_tb");
 const _select_type = require("./select_type");
-const _select_type_tb = require("./select_type_tb");
 const _users = require("./users");
 
 function initModels(sequelize) {
@@ -12,14 +12,14 @@ function initModels(sequelize) {
   const department = _department(sequelize, DataTypes);
   const department_tb = _department_tb(sequelize, DataTypes);
   const persionality = _persionality(sequelize, DataTypes);
+  const persionality_tb = _persionality_tb(sequelize, DataTypes);
   const select_type = _select_type(sequelize, DataTypes);
-  const select_type_tb = _select_type_tb(sequelize, DataTypes);
   const users = _users(sequelize, DataTypes);
 
   department_tb.belongsTo(booking_info, { as: "id_booking_booking_info", foreignKey: "id_booking"});
   booking_info.hasMany(department_tb, { as: "department_tbs", foreignKey: "id_booking"});
-  select_type_tb.belongsTo(booking_info, { as: "id_booking_booking_info", foreignKey: "id_booking"});
-  booking_info.hasMany(select_type_tb, { as: "select_type_tbs", foreignKey: "id_booking"});
+  persionality_tb.belongsTo(booking_info, { as: "id_booking_booking_info", foreignKey: "id_booking"});
+  booking_info.hasMany(persionality_tb, { as: "persionality_tbs", foreignKey: "id_booking"});
   booking_info.belongsTo(users, { as: "id_user_user", foreignKey: "id_user"});
   users.hasMany(booking_info, { as: "booking_infos", foreignKey: "id_user"});
   department.belongsTo(users, { as: "id_user_user", foreignKey: "id_user"});
@@ -34,8 +34,8 @@ function initModels(sequelize) {
     department,
     department_tb,
     persionality,
+    persionality_tb,
     select_type,
-    select_type_tb,
     users,
   };
 }
---
+++ src/models/persionality_tb.js
@@ -0,0 +1,50 @@
+const Sequelize = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  return persionality_tb.init(sequelize, DataTypes);
+}
+
+class persionality_tb extends Sequelize.Model {
+  static init(sequelize, DataTypes) {
+  return super.init({
+    id_selection: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    _values: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    id_booking: {
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: 'booking_info',
+        key: 'id_booking'
+      }
+    }
+  }, {
+    sequelize,
+    tableName: 'persionality_tb',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id_selection" },
+        ]
+      },
+      {
+        name: "id_booking",
+        using: "BTREE",
+        fields: [
+          { name: "id_booking" },
+        ]
+      },
+    ]
+  });
+  }
+}
