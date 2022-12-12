4645a9a8d3203ca679342e7fde6357e2a7d87b18
PhuforWork <vqphu97@gmail.com>
2022-12-12 19:58:19 +0700

    update7

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -208,12 +208,12 @@ const delete_bk = async (req, res) => {
   const check = await model.booking_info.findByPk(id);
   if (check) {
     console.log(123);
+    res.send("success delete");
   }
 
-  await model.booking_info.destroy({ where: { id_booking: id } });
-  await model.department_tb.destroy({ where: { id_booking: id } });
-  await model.persionality_tb.destroy({ where: { id_booking: id } });
-  res.send("success delete");
+  // await model.booking_info.destroy({ where: { id_booking: id } });
+  // await model.department_tb.destroy({ where: { id_booking: id } });
+  // await model.persionality_tb.destroy({ where: { id_booking: id } });
 };
 
 module.exports = {
