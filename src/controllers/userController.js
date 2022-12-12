f82ec3def922c599c3b7062b3b3af25c83e6e5df
PhuforWork <vqphu97@gmail.com>
2022-12-12 16:35:58 +0700

    update7

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -21,7 +21,6 @@ const getUserId = async (req, res) => {
   let data_bk = await model.booking_info.findAll({
     include: ["department_tbs"],
   });
-  let select_type_bk = await model.select_type_tb.findAll();
   let user_name = data.user_name;
   let _password = data._password;
   let id_user = data.id_user;
@@ -37,7 +36,6 @@ const getUserId = async (req, res) => {
     persionalities,
     select_types,
     data_bk,
-    select_type_bk
   });
 };
 // Login user
