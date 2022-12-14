8c7333ea32cbb8946286676360c318cdd36b3ad6
PhuforWork <vqphu97@gmail.com>
2022-12-14 16:40:05 +0700

    up1

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -60,7 +60,6 @@ const add_booking = async (req, res) => {
     Promise.all(
       personality.map((values) => {
         model.persionality_tb.create({
-          id_per: idbk.id_booking,
           value: values.value,
           label: values.label,
           id_booking: idbk.id_booking,
