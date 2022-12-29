e038b21045550f8a8f3fb96f7be8e11367e78a48
PhuforWork <vqphu97@gmail.com>
2022-12-29 14:05:55 +0700

    update

--- src/untils/respone.js
+++ src/untils/respone.js
@@ -6,7 +6,7 @@ const successCode = (res, data, message) => {
 };
 
 const failCode = (res, data, message) => {
-  res.status(401).json({
+  res.status(422).json({
     message,
     content: data,
   });
