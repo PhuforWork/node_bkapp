fd0e908ea38fa53825beeb8e34f8f7c6f5fc3706
PhuforWork <vqphu97@gmail.com>
2022-12-31 09:59:54 +0700

    up date

---
+++ src/Middlewares/auth.js
@@ -0,0 +1,26 @@
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
+
+//ma hoa du lieu
+const encodeTokenEmail = (data) => {
+  let token = jwt.sign(data, process.env.ENDCODE_EMAIL, {
+    expiresIn: "5m",
+  });
+  return token;
+};
+
+//check token hop le
+const compareToken = (token) => {
+  const veryfyToken = jwt.verify(token, process.env.ENDCODE_EMAIL);
+  if (veryfyToken) {
+    return veryfyToken;
+  } else {
+    return false;
+  }
+};
+//giãi mã
+const decodeToken = (token) => {
+  return jwt.decode(token);
+};
+
+module.exports = { encodeTokenEmail, compareToken, decodeToken };
