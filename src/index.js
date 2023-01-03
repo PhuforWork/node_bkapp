8c100402b83333d6cb81308d4f56897a79d97dad
PhuforWork <vqphu97@gmail.com>
2023-01-03 17:39:34 +0700

    up date

--- src/index.js
+++ src/index.js
@@ -21,13 +21,38 @@ app.get("/test", (req, res) => {
   res.send("Hello");
 });
 
+let onlineUser = [];
+const addNewUser = (user_name, socketId) => {
+  !onlineUser.some((user) => user.user_name === user_name) &&
+    onlineUser.push({ user_name, socketId });
+};
+
+const removeUser = (socketId) => {
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (user_name) => {
+  return onlineUser.find((user) => user.user_name === user_name);
+};
+
 io.on("connection", (socket) => {
-  console.log("user-connect", socket);
-  io.emit("user-connect", socket.id);
+  // add user
+  socket.on("newUser", (user_name) => {
+    addNewUser(user_name, socket.id);
+  });
+
+  //send notification
+  socket.on("sendNotification", ({ senderName, receiverName, type }) => {
+    const receiver = getUser(receiverName);
+    io.to(receiver.socketId).emit("getNotification", {
+      senderName,
+      type,
+    });
+  });
 
   // disconnect
   socket.on("disconnect", (reason) => {
-    io.emit("user-disconnect", socket.id);
+    removeUser(socket.id);
   });
 });
 
