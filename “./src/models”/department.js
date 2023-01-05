1afbeb051d348f157fe96a802bce43d18e22497d
PhuforWork <vqphu97@gmail.com>
2023-01-05 13:11:04 +0900

    up

---
+++ “./src/models”/department.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  return department.init(sequelize, DataTypes);
+}
+
+class department extends Sequelize.Model {
+  static init(sequelize, DataTypes) {
+  return super.init({
+    id_derp: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    label: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    id_user: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id_user'
+      }
+    },
+    value: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    tableName: 'department',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id_derp" },
+        ]
+      },
+      {
+        name: "id_user",
+        using: "BTREE",
+        fields: [
+          { name: "id_user" },
+        ]
+      },
+    ]
+  });
+  }
+}
---
+++ “./src/models”/select_type.js
@@ -0,0 +1,54 @@
+const Sequelize = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  return select_type.init(sequelize, DataTypes);
+}
+
+class select_type extends Sequelize.Model {
+  static init(sequelize, DataTypes) {
+  return super.init({
+    id_sl: {
+      autoIncrement: true,
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true
+    },
+    _values: {
+      type: DataTypes.STRING(255),
+      allowNull: true
+    },
+    id_user: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: 'users',
+        key: 'id_user'
+      }
+    },
+    id_selection: {
+      type: DataTypes.DOUBLE,
+      allowNull: false
+    }
+  }, {
+    sequelize,
+    tableName: 'select_type',
+    timestamps: false,
+    indexes: [
+      {
+        name: "PRIMARY",
+        unique: true,
+        using: "BTREE",
+        fields: [
+          { name: "id_sl" },
+        ]
+      },
+      {
+        name: "id_user",
+        using: "BTREE",
+        fields: [
+          { name: "id_user" },
+        ]
+      },
+    ]
+  });
+  }
+}
