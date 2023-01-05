0c24ed1081b47512a0c920c22a356f73be05397b
PhuforWork <vqphu97@gmail.com>
2023-01-05 18:06:43 +0700

    update

--- src/index.js
+++ src/index.js
@@ -13,6 +13,9 @@ const io = new Server(httpServer, { cors: { origin: "*" } });
 app.use(express.json());
 app.use(cors());
 app.use(express.static("."));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 httpServer.listen(8081);
 
 app.get("/test", (req, res) => {
