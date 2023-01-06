a6b7aec42604425bf46fc7f4f0c900c434c18131
PhuforWork <vqphu97@gmail.com>
2023-01-06 16:54:11 +0700

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -734,6 +734,7 @@ const notification_get = async (req, res) => {
   try {
     let { id } = req.params;
     const notifi_get = await model.notifications.findAll({
+      include: ["persionality_notifies"],
       where: { id_user: id },
     });
     successCode(res, notifi_get, "Success get notify");
