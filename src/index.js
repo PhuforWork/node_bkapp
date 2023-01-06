511263b09d203d8d059f3a3b73dde7ca66da8eda
PhuforWork <vqphu97@gmail.com>
2023-01-06 14:37:33 +0700

    up

--- src/index.js
+++ src/index.js
@@ -13,8 +13,7 @@ const io = new Server(httpServer, { cors: { origin: "*" } });
 app.use(express.json());
 app.use(cors());
 app.use(express.static("."));
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+
 
 httpServer.listen(8081);
 
