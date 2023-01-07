0a7253d77e0e1aa2ed432a34dc090b7419f95267
PhuforWork <vqphu97@gmail.com>
2023-01-07 18:55:07 +0700

    change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -776,7 +776,7 @@ const notification_delete = async (req, res) => {
 //báo thuc khi lich toi hen
 const alarm_immediately = async (data) => {
 
-  let datetimeLocal = moment.utc(data.date).toLocaleString();
+  let datetimeLocal = moment(data.date);
 
   console.log(data.date);
   console.log(datetimeLocal);
