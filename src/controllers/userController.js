1b69272ee25e3160caf676e9ed05e6b717ccb464
PhuforWork <vqphu97@gmail.com>
2023-01-07 16:47:41 +0700

    change

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -780,13 +780,13 @@ const alarm_immediately = async (data) => {
   console.log(datetimeUTC);
   console.log(datetimeLocal);
 
-  let DD = new Date(datetime).getDate();
-  let MM = new Date(datetime).getMonth()+1;
-  let hh = new Date(datetime).getHours();
-  let mm = new Date(datetime).getMinutes();
-  let ss = new Date(datetime).getSeconds();
-  let YYYY = new Date(datetime).getFullYear();
-  console.log(DD,MM,hh,mm,ss);
+  // let DD = new Date(datetime).getDate();
+  // let MM = new Date(datetime).getMonth()+1;
+  // let hh = new Date(datetime).getHours();
+  // let mm = new Date(datetime).getMinutes();
+  // let ss = new Date(datetime).getSeconds();
+  // let YYYY = new Date(datetime).getFullYear();
+  // console.log(DD,MM,hh,mm,ss);
 
   const date_new = new Date(YYYY,MM)
   await schedule.scheduleJob(datetime, () => {
