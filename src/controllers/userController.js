eeb1595b1dac3eefcd4687596eeda729e3be4b0c
PhuforWork <vqphu97@gmail.com>
2023-01-09 16:13:13 +0700

    up

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -792,8 +792,8 @@ const alarm_immediately = async (data) => {
   rule.year = 2023;
   rule.month = 1;
   rule.date = 9;
-  rule.hour = 18;
-  rule.minute = 12;
+  rule.hour = 14;
+  rule.minute = 14;
   rule.second = 1;
   console.log(rule);
   await schedule.scheduleJob(rule, () => {
