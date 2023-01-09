b239593ffc7961f597b39f366a93fbeff2f7a753
PhuforWork <vqphu97@gmail.com>
2023-01-09 15:32:24 +0700

    up

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -788,7 +788,7 @@ const alarm_immediately = async (data) => {
   let YYYY = datetimeLocal.year();
   let ss = datetimeLocal.seconds();
   console.log(DD, MM, YYYY, hh, mm);
-  cron.schedule(`${ss} ${mm} ${hh} ${DD} ${MM} *`, () => {
+  schedule.scheduleJob(`${ss} ${mm} ${hh} ${DD} ${MM} *`, () => {
     console.log("testoooooo", 12341);
   });
 };
