c14b5003d41055265aed4b6b15b664375bca07f8
PhuforWork <vqphu97@gmail.com>
2023-01-10 14:08:40 +0700

    update

--- src/eventSocket/alarmSocket.js
+++ src/eventSocket/alarmSocket.js
@@ -3,16 +3,16 @@ const moment = require("moment");
 const { notification_alarm } = require("../controllers/userController");
 module.exports = (io) => {
   //báo thuc khi lich toi hen
-  let arlamBooking = [];
+  let alarmBooking = [];
   const alarm_immediately = async (data) => {
     let datetimeLocal = moment(data.start);
     let today = new Date();
     // let test = moment().format("Z");
-    await arlamBooking.push(data);
-    // console.log("1",arlamBooking);
+    await alarmBooking.push(data);
+    // console.log("1",alarmBooking);
     // console.log("loggggggggggg", datetimeLocal);
     Promise.all(
-      arlamBooking.map(async (ele) => {
+      alarmBooking.map(async (ele) => {
         let MM = (await ele.start.month()) + 1;
         let DD = await ele.start.date();
         let hh = await ele.start.hours();
@@ -24,17 +24,18 @@ module.exports = (io) => {
         // let hh = 16;
         // let mm = 5;
         // let ss = 1;
-
+        console.log("1", alarmBooking);
         console.log(hh, mm, ss, DD, MM);
         await schedule.scheduleJob(
           `${ss} ${mm} ${hh} ${DD} ${MM} *`,
           async () => {
             await io.emit("sendAlarm", { ...data, today: today });
-            console.log("testSend", 123);
+            // console.log("testSend", 123);
             notification_alarm({ ...data, today: today });
-            arlamBooking = await arlamBooking.filter(
+            alarmBooking = await alarmBooking.filter(
               (ele1) => ele1.start !== ele.start
             );
+            console.log("2", alarmBooking);
           }
         );
       })
