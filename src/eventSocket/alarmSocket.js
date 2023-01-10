831090117f1402763af7380c09d0ec616fb78d82
PhuforWork <vqphu97@gmail.com>
2023-01-10 18:10:53 +0700

    update

--- src/eventSocket/alarmSocket.js
+++ src/eventSocket/alarmSocket.js
@@ -10,40 +10,34 @@ module.exports = (io) => {
     await alarmBooking.push(datetimeLocal);
     // console.log("1",alarmBooking);
     // console.log("loggggggggggg", datetimeLocal);
-    let today;
-    const test = setInterval(() => {
-      today = moment();
-      clearInterval(test);
-    }, 1000);
+    let today = moment();
 
     console.log("today", today);
     Promise.all(
       alarmBooking.map(async (ele) => {
-        if (ele === today) {
-          let MM = (await ele.month()) + 1;
-          let DD = await ele.date();
-          let hh = await (ele.minutes() === 0 ? ele.hours() - 1 : ele.hours());
-          let mm = await (ele.minutes() === 0 ? 55 : ele.minutes() - 5);
-          let ss = (await ele.second()) * 0 + 1;
-          //
-          // let MM = 1;
-          // let DD = 10;
-          // let hh = 16;
-          // let mm = 5;
-          // let ss = 1;
-          console.log("array before", alarmBooking);
-          console.log("show datetime", hh, mm, ss, DD, MM);
-          await schedule.scheduleJob(
-            `${ss} ${mm} ${hh} ${DD} ${MM} *`,
-            async () => {
-              await io.emit("sendAlarm", { ...data, today: today });
-              // console.log("testSend", 123);
-              notification_alarm({ ...data, today: today });
-              alarmBooking = await alarmBooking.filter((ele1) => ele1 !== ele);
-              console.log("alarm after", alarmBooking);
-            }
-          );
-        }
+        let MM = (await ele.month()) + 1;
+        let DD = await ele.date();
+        let hh = await (ele.minutes() === 0 ? ele.hours() - 1 : ele.hours());
+        let mm = await (ele.minutes() === 0 ? 55 : ele.minutes() - 5);
+        let ss = (await ele.second()) * 0 + 1;
+        //
+        // let MM = 1;
+        // let DD = 10;
+        // let hh = 16;
+        // let mm = 5;
+        // let ss = 1;
+        console.log("array before", alarmBooking);
+        console.log("show datetime", hh, mm, ss, DD, MM);
+        await schedule.scheduleJob(
+          `${ss} ${mm} ${hh} ${DD} ${MM} *`,
+          async () => {
+            await io.emit("sendAlarm", { ...data, today: today });
+            // console.log("testSend", 123);
+            notification_alarm({ ...data, today: today });
+            alarmBooking = await alarmBooking.filter((ele1) => ele1 !== ele);
+            console.log("alarm after", alarmBooking);
+          }
+        );
       })
     );
   };
