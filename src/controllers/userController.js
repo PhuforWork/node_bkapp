2f90c46fc9f13556949d8d3bd850870bfbbf4175
PhuforWork <vqphu97@gmail.com>
2023-01-10 10:37:08 +0700

    update

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -784,11 +784,18 @@ const alarm_immediately = async (data) => {
   // console.log("loggggggggggg", datetimeLocal);
   Promise.all(
     arlamBooking.map(async (ele) => {
-      let MM = (await ele.start.month()) + 1;
-      let DD = await ele.start.date();
-      let hh = await ele.start.hours();
-      let mm = await ele.start.minutes();
-      let ss = (await ele.start.second()) * 0 + 1;
+      // let MM = (await ele.start.month()) + 1;
+      // let DD = await ele.start.date();
+      // let hh = await ele.start.hours();
+      // let mm = await ele.start.minutes();
+      // let ss = (await ele.start.second()) * 0 + 1;
+      // 
+      let MM = 1;
+      let DD = 10;
+      let hh = 12;
+      let mm = 39;
+      let ss = 1;
+
       console.log(hh, mm, ss, DD, MM);
       await schedule.scheduleJob(
         `${ss} ${mm} ${hh} ${DD} ${MM} *`,
