d93cd381686d51dea707cbc0ca51258caad737b9
PhuforWork <vqphu97@gmail.com>
2023-01-16 08:34:49 +0700

    up1

--- src/eventSocket/alarmSocket.js
+++ src/eventSocket/alarmSocket.js
@@ -12,11 +12,18 @@ module.exports = (io) => {
 
     Promise.all(
       alarmBooking.map(async (ele) => {
-        let MM = (await ele.date_early_5.month()) + 1;
-        let DD = await ele.date_early_5.date();
-        let hh = await ele.date_early_5.hours();
-        let mm = await ele.date_early_5.minutes();
-        let ss = (await ele.date_early_5.second()) * 0 + 1;
+        let data1 = alarmBooking[0];
+        // let MM = (await ele.date_early_5.month()) + 1;
+        // let DD = await ele.date_early_5.date();
+        // let hh = await ele.date_early_5.hours();
+        // let mm = await ele.date_early_5.minutes();
+        // let ss = (await ele.date_early_5.second()) * 0 + 1;
+        //
+        let MM = (await data1.date_early_5.month()) + 1;
+        let DD = await data1.date_early_5.date();
+        let hh = await data1.date_early_5.hours();
+        let mm = await data1.date_early_5.minutes();
+        let ss = (await data1.date_early_5.second()) * 0 + 1;
         //
         // let MM = 1;
         // let DD = 10;
@@ -29,14 +36,14 @@ module.exports = (io) => {
         await schedule.scheduleJob(
           `${ss} ${mm} ${hh} ${DD} ${MM} *`,
           async () => {
-            let data1 = alarmBooking[0];
+            // let data1 = alarmBooking[0];
             let today = moment();
             console.log("array[0]", data1);
-            await io.emit("sendAlarm");
+            // await io.emit("sendAlarm");
             io.emit("getNotification");
             notification_alarm({ ...data1, today: today });
             alarmBooking = await alarmBooking.filter(
-              (ele1) => ele1.start !== ele.start
+              (ele1) => ele1.start !== ele[0].start
             );
             console.log("alarm after", alarmBooking);
             console.log("array[0]", data1);
