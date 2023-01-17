632969d69e089df83b065f37462acfc75fbdccef
PhuforWork <vqphu97@gmail.com>
2023-01-17 10:32:57 +0700

    up1

--- src/eventSocket/alarmSocket.js
+++ src/eventSocket/alarmSocket.js
@@ -26,18 +26,21 @@ module.exports = (io) => {
 
         console.log("array before", alarmBooking);
         console.log("show datetime", hh, mm, ss, DD, MM);
+        let data1 = alarmBooking[0];
         await schedule.scheduleJob(
+          data1.start,
           `${ss} ${mm} ${hh} ${DD} ${MM} *`,
           async () => {
-            // let data1 = alarmBooking[0];
             let today = moment();
             console.log("array[0]", data1);
             // await io.emit("sendAlarm");
             io.emit("getNotification");
-            notification_alarm({ ...alarmBooking[0], today: today });
-            alarmBooking = await alarmBooking.filter(
-              (ele1) => moment(ele1.start) !== moment(ele.start)
-            );
+            notification_alarm({ ...data1, today: today });
+            schedule.cancelJob(data1.start);
+            alarmBooking.splice(0, 1);
+            // alarmBooking = await alarmBooking.filter(
+            //   (ele1) => moment(ele1.start) !== moment(ele.start)
+            // );
             console.log("alarm after", alarmBooking);
             console.log("array[0]", data1);
           }
