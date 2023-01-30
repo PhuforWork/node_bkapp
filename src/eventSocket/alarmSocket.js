b9341dae825e8e0beb1b3de5b6f5eed5bc8be690
PhuforWork <vqphu97@gmail.com>
2023-01-30 09:44:27 +0700

    update 1

--- src/eventSocket/alarmSocket.js
+++ src/eventSocket/alarmSocket.js
@@ -4,8 +4,7 @@ const init_models = require("../models/init-models");
 const model = init_models(sequelize);
 const moment = require("moment");
 const { notification_alarm } = require("../controllers/userController");
-// const async = require("async");
-let alarmBooking = [];
+
 module.exports = (io) => {
   //báo thuc khi lich toi hen
   const alarm_immediately = async () => {
@@ -16,9 +15,8 @@ module.exports = (io) => {
     });
     Data = JSON.parse(JSON.stringify(Data));
     // let test = moment().format("Z");
-    // await alarmBooking.push({ ...data, date_early_5: datetimeLocal });
 
-    // console.log("array before", alarmBooking);
+    console.log("array before", Data);
     Promise.all(
       Data.map(async (ele) => {
         let alarmDate = await moment(ele.alarmDate);
@@ -32,20 +30,14 @@ module.exports = (io) => {
           `${ss} ${mm} ${hh} ${DD} ${MM} *`,
           async () => {
             let today = moment();
-            // await io.emit("sendAlarm");
             await io.emit("getNotification");
-
             await notification_alarm({
               ...ele,
               today: today,
               type: 2,
               status: false,
             });
-            // alarmBooking = await alarmBooking.filter(
-            //   (ele1) => ele1.start !== ele.start
-            // );
             await io.emit("getNotification");
-            // console.log("alarm after", alarmBooking);
           }
         );
       })
