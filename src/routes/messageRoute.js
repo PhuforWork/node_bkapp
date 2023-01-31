2ecbec9278dfef51da0935ae1085adeb6e70e3bf
PhuforWork <vqphu97@gmail.com>
2023-01-31 16:39:41 +0700

    up1

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -9,11 +9,12 @@ const {
 } = require("../controllers/messageController");
 const upload = require("../Middlewares/upload");
 const messRoute = express.Router();
+let multipleUpload = upload.fields([{name:'images',maxCount:100}])
 
 messRoute.get("/get-contacts", get_contact_messs);
 
 messRoute.post("/send-mess", upload.none(), send_mess);
-messRoute.post("/send-files", upload.array('files',100), send_files);
+messRoute.post("/send-files", multipleUpload, send_files);
 messRoute.post("/send-images", upload.single('images'), send_media);
 messRoute.post("/send-links", upload.none(), send_links);
 
