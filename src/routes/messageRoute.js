ddb42faa74c364ca575f9945415137833b9fda12
PhuforWork <vqphu97@gmail.com>
2023-01-31 14:12:31 +0700

    up1

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -14,7 +14,7 @@ messRoute.get("/get-contacts", get_contact_messs);
 
 messRoute.post("/send-mess", upload.none(), send_mess);
 messRoute.post("/send-files", upload.array('files',100), send_files);
-messRoute.post("/send-images", upload.single('images'), send_media);
+messRoute.post("/send-images", upload.array('images',100), send_media);
 messRoute.post("/send-links", upload.none(), send_links);
 
 messRoute.delete("/send-mess", delete_mes);
