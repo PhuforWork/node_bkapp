14b209897664535565fdbd08e4757a71b56cfaed
PhuforWork <vqphu97@gmail.com>
2023-02-01 10:13:38 +0700

    update

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -47,7 +47,7 @@ const send_media = async (req, res) => {
     let { id } = req.params; //id_user
     let data = req.files;
     console.log("daaatssa",data);
-    let image_url = "http://110.35.173.82:8081" + "/" + req.files.path;
+    let image_url = "http://110.35.173.82:8081" + "/" + req.file.path;
     // await model.media_message.create({ image_url, id_user: id });
     successCode(res, image_url, "Success");
   } catch (error) {
