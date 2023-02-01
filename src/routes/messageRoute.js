1d1030323291f4d7e848189ac98dd0271751e274
PhuforWork <vqphu97@gmail.com>
2023-02-01 13:41:36 +0700

    update

--- src/routes/messageRoute.js
+++ src/routes/messageRoute.js
@@ -16,8 +16,8 @@ const messRoute = express.Router();
 messRoute.get("/get-contacts", get_contact_messs);
 
 messRoute.post("/send-mess", upload.none(), send_mess);
-messRoute.post("/send-images", upload1.array("images"), send_media);
-messRoute.post("/send-files", upload2.array("files"), send_files);
+messRoute.post("/send-images/:id", upload1.array("images"), send_media);
+messRoute.post("/send-files/:id", upload2.array("files"), send_files);
 messRoute.post("/send-links", upload.none(), send_links);
 
 messRoute.delete("/delete-mess", delete_mes);
