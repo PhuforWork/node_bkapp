2aff9911625534a1654ed0b5a4736adaf5754aba
PhuforWork <vqphu97@gmail.com>
2023-02-02 10:51:13 +0700

    update

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -14,7 +14,7 @@ const get_all_contact = async (req, res) => {
       attributes: { exclude: ["_password", "email"] },
     });
     getAllContact = JSON.parse(JSON.stringify(getAllContact));
-    getAllContact = getAllContact.filter((ele) => console.log(ele.id_user));
+    getAllContact = getAllContact.filter((ele) => ele.id_user == id);
     console.log(getAllContact);
     successCode(res, getAllContact, "Success");
   } catch (error) {
