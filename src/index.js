9ead0076ef04c843fb98b9302afc3e0059491b3c
PhuforWork <vqphu97@gmail.com>
2023-02-02 15:20:41 +0700

    avvv

--- src/index.js
+++ src/index.js
@@ -65,7 +65,7 @@ io.on("connection", (socket) => {
   socket.on(
     "sendNotification",
     async ({ senderName, receiverName, type, status, id_user, data }) => {
-      const receiver = getUser(receiverName);
+      // const receiver = getUser(receiverName);
       await io.emit("getNotification");
       await alarm_immediately();
       await io.emit("getNotification");
