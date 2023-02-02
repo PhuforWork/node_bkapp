5477c16a1d9867e3d9b4819281f3adae7d94d03d
PhuforWork <vqphu97@gmail.com>
2023-02-02 15:09:48 +0700

    avvv

--- src/index.js
+++ src/index.js
@@ -45,11 +45,8 @@ const removeUser = (socketId) => {
 };
 
 const getUser = (id_user_receive) => {
-  console.log("1",id_user_receive);
-  return onlineUser.filter((ele) => {
-    console.log("2",ele);
-    return ele.id_user === id_user_receive
-  });
+  console.log("1",id_user_receive,onlineUser.filter((ele) => ele.id_user === id_user_receive));
+  return onlineUser.filter((ele) => ele.id_user === id_user_receive)
 };
 
 io.on("connection", (socket) => {
