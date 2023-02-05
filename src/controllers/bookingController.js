1fe8f92c7cf95021e5ffbdb865b084ff2a29e9d5
dai nguyen <dainguyen020199@gmail.com>
2023-02-06 04:57:52 +0700

    #eric update

--- src/controllers/bookingController.js
+++ src/controllers/bookingController.js
@@ -495,18 +495,16 @@ const updateNotifyByBookingUpdate = (checkbk, start, end, label, personality) =>
         await model.notifications.update(getNotifyUpdate, { where: { checkbk: getNotifyUpdate.checkbk } });
       }
       if (getPersonalUpdate.length) {
-        Promise.all(
-          await model.persionality_notify.destroy({
-            where: { id_notify: getNotifyUpdate.id_notify }
-          }),
-          await personality.forEach(async (item) => {
-            await model.persionality_notify.create({
-              label: item.label,
-              id_notify: getNotifyUpdate.id_notify,
-              value: item.value
-            });
-          })
-        );
+        await model.persionality_notify.destroy({
+          where: { id_notify: getNotifyUpdate.id_notify }
+        })
+        await personality.forEach(async (item) => {
+          await model.persionality_notify.create({
+            label: item.label,
+            id_notify: getNotifyUpdate.id_notify,
+            value: item.value
+          });
+        })
       }
       resolve();
     } catch (e) {
