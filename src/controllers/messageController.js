bd28341496439b492ae127fb78f101310fe0320e
PhuforWork <vqphu97@gmail.com>
2023-02-06 19:48:51 +0700

    up change

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -16,10 +16,9 @@ const get_all_contact = async (req, res) => {
     getAllContact = await JSON.parse(JSON.stringify(getAllContact));
     let getAllNewContact = getAllContact.filter((ele) =>
       ele.content_messages.some(
-        (ele) =>
-          ele.id_user_send === id_send * 1 
-          // &&
-          // ele.id_user_receive == id_receive * 1
+        (ele) => ele.id_user_send === id_send * 1
+        // &&
+        // ele.id_user_receive == id_receive * 1
       )
     );
     successCode(res, getAllNewContact, "Success");
@@ -32,7 +31,7 @@ const get_contact_messs = async (req, res) => {
   let { id_send, id_receive } = req.params; //id user
   console.log("params", { id_send, id_receive });
   try {
-    const get_id_Contact = await model.users.findAll({
+    let get_id_Contact = await model.users.findAll({
       include: [
         "select_types",
         "persionalities",
@@ -41,10 +40,13 @@ const get_contact_messs = async (req, res) => {
         "media_messages",
         "links_messages",
       ],
-      where: { id_user: id_send },
       attributes: { exclude: ["_password", "email"] },
     });
-    successCode(res, get_id_Contact, "Get Success");
+    get_id_Contact = await JSON.parse(JSON.stringify(get_id_Contact));
+    let get_contact_by = getAllContact.filter((ele) =>
+      ele.content_messages.some((ele) => ele.id_user_send === id_send * 1)
+    );
+    successCode(res, get_contact_by, "Get Success");
   } catch (error) {
     errorCode(res, "Error BackEnd");
   }
