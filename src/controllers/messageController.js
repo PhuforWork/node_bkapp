6b3d36bfa3c51ff146aa05e3b1e9a9652f116c49
PhuforWork <vqphu97@gmail.com>
2023-02-06 19:54:15 +0700

    up change

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -47,6 +47,8 @@ const get_contact_messs = async (req, res) => {
       ele.content_messages.some(
         (ele) =>
           ele.id_user_send === id_send * 1 &&
+          ele.id_user_send === id_receive * 1 &&
+          ele.id_user_receive === id_send * 1 &&
           ele.id_user_receive === id_receive * 1
       )
     );
