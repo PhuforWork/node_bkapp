639af0aecba2cf7b877b85122333f645493c38a0
PhuforWork <vqphu97@gmail.com>
2023-02-07 14:06:44 +0700

    up

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -21,8 +21,8 @@ const get_all_contact = async (req, res) => {
         ele.content_messages.map((ele) => {
           if (
             (ele.id_user_send == id_send &&
-              ele.id_user_receive == ele.group - id_send) ||
-            (ele.id_user_send == ele.group - id_send &&
+              ele.id_user_receive == ele.id_user) ||
+            (ele.id_user_send == ele.id_user &&
               ele.id_user_receive == id_send)
           ) {
             return ele;
