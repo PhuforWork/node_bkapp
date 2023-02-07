8e5c647e3c3e149d5e266d3a47a7f00e2b1dcf29
PhuforWork <vqphu97@gmail.com>
2023-02-07 13:13:19 +0700

    up

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -15,7 +15,7 @@ const get_all_contact = async (req, res) => {
       attributes: { exclude: ["_password", "email"] },
     });
     getAllContact = await JSON.parse(JSON.stringify(getAllContact));
-    // let getContact = getAllContact.filter((ele) => ele.id_user != id_send);
+    getAllContact = getAllContact.filter((ele) => ele.id_user != id_send);
     // let get_contact = getContact.filter((ele) =>
     //   ele.content_message.some((ele) => ele.id_user_send === id_send)
     // );
