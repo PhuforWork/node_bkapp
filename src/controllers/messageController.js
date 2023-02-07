dc24bb8053f145d06ffd48e0c80ea0c2f213d092
PhuforWork <vqphu97@gmail.com>
2023-02-07 10:29:47 +0700

    up

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -64,7 +64,7 @@ const get_contact_messs = async (req, res) => {
     //     (ele.id_user_send == id_send && ele.id_user_receive == id_receive) ||
     //     (ele.id_user_send == id_receive && ele.id_user_receive == id_send)
     // );
-    successCode(res, { ...infor_receive, getContact }, "Get Success");
+    successCode(res, getContact, "Get Success");
   } catch (error) {
     errorCode(res, "Error BackEnd");
   }
