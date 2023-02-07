d84dc233528e662b2cd62ddede4f51ed9299691e
PhuforWork <vqphu97@gmail.com>
2023-02-07 10:46:36 +0700

    up

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -17,11 +17,6 @@ const get_all_contact = async (req, res) => {
     // let getAllNewContact = getAllContact.filter((ele) =>
     //   ele.content_messages.some((ele) => ele.id_user_send == id_send)
     // );
-
-
-
-
-
     successCode(res, getAllContact, "Success");
   } catch (error) {
     errorCode(res, "Error BackEnd");
