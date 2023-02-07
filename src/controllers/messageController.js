a531e8ef71dd6cee40c95387e587bd81a6c0c367
PhuforWork <vqphu97@gmail.com>
2023-02-07 13:39:49 +0700

    up

--- src/controllers/messageController.js
+++ src/controllers/messageController.js
@@ -16,7 +16,11 @@ const get_all_contact = async (req, res) => {
     });
     getAllContact = await JSON.parse(JSON.stringify(getAllContact));
     getAllContact = getAllContact.filter((ele) => ele.id_user != id_send);
-
+    let get_contact = getAllContact.filter((ele) =>
+      ele.content_messages.filter(
+        (ele1) => ele1.group == id_send + ele1.id_user_receive
+      )
+    );
     // let get_contact = getAllContact.filter(
     //   async (ele) =>
     //     (ele.content_messages.id_send == id_send &&
@@ -26,16 +30,7 @@ const get_all_contact = async (req, res) => {
     //       ele.content_messages.group - id_send * 1 &&
     //       ele.content_messages.id_receve == id_send)
     // );
-    let getContact = await model.content_message.findAll({
-      where: { id_user: id_send },
-    });
-    getContact = await JSON.parse(JSON.stringify(getContact));
-    let get_contact = getContact.filter(
-      (ele) =>
-        (ele.id_user_send == id_send && ele.id_user_receive == id_receive) ||
-        (ele.id_user_send == id_receive && ele.id_user_receive == id_send)
-    );
-    successCode(res, { ...getAllContact, get_contact }, "Success");
+    successCode(res, get_contact, "Success");
   } catch (error) {
     errorCode(res, "Error BackEnd");
   }
